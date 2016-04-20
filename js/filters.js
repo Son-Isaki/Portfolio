5c5a2e5bb97c9de88f0f4ddfdbc73b2b58edb1e2
Romain Tiger <romaintiger@orange.fr>
2016-04-20 22:21:38 +0200

    Mise en place du systeme de filtrage des projets

--- js/filters.js
+++ js/filters.js
@@ -6,9 +6,57 @@ $(document).ready(function () {
     // Les variables
     var filters = $(".filters > button");
 
-    // Au clique sur le hamburger
+    // Au clique sur un filtre
     filters.click(function () {
         filters.removeClass("selected");
         $(this).addClass("selected");
+
+        updateContent($(this).data("filter"));
     });
 });
+
+function updateContent(value, isMobile) {
+    if (isMobile) {
+        setDesktopFilter(value);
+    }
+    else {
+        setMobileFilter(value);
+    }
+
+    doFiltering(value);
+}
+
+function setMobileFilter(value) {
+    $(".select-filters > .cs-options > ul > li").each(function () {
+        if ($(this).data("value") == value) {
+            $(this).addClass("cs-selected");
+
+            var text = $(this).find("span").html();
+            $(".select-filters > .cs-placeholder").html(text);
+        }
+        else {
+            $(this).removeClass("cs-selected");
+        }
+    });
+}
+
+function setDesktopFilter(value) {
+    $(".filters > button").each(function () {
+        if ($(this).data("filter") == value) {
+            $(this).addClass("selected");
+        }
+        else {
+            $(this).removeClass("selected");
+        }
+    });
+}
+
+function doFiltering(value) {
+    if (value == "all") {
+        $(".gallery > ul > li").show();
+    }
+    else {
+        $(".gallery > ul > li").hide();
+        $(".gallery > ul > li." + value).show();
+    }
+}
